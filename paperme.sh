9eb5d13f1a209394f0efb0e30ae54bc58a8fe57f
Eli Dickinson <eli@industrydive.com>
2021-03-24 12:57:48 -0400

    get whole paper info line and put in footer

--- paperme.sh
+++ paperme.sh
@@ -4,7 +4,8 @@
 # DC_WP - Wapo
 # cd "${0%/*}"
 PAPER_ID=${1:-`shuf -n 1 paperindex.tsv | awk '{print $1}'`}
-echo "$PAPER_ID"
+PAPER_INFO=`grep "$PAPER_ID" paperindex.tsv`
+echo "$PAPER_INFO"
 wget "https://cdn.freedomforum.org/dfp/pdf`date "+%-d"`/$PAPER_ID.pdf" -q -O $(pwd)/$PAPER_ID.pdf
 echo "Render pdf to image"
 # echo docker run -it --rm -v $(pwd):/var/workdir/ kolyadin/pdf2img -png -singlefile -cropbox $1.pdf $1
@@ -12,6 +13,6 @@ echo "Render pdf to image"
 pdftoppm -png -singlefile -cropbox $PAPER_ID.pdf $PAPER_ID
 echo "Resize and annotate image"
 mogrify -gravity north -depth 8 -resize 1440x2560 -background white -extent 1440x2560 \
-    -undercolor '#00000020' -fill black -gravity South -pointsize 18 -annotate +0+5 "  $PAPER_ID -- `date`  " -depth 8 $PAPER_ID.png
+    -font TimesNewRoman -undercolor '#F0F0F090' -fill black -gravity South -pointsize 18 -annotate +0+5 "\t $PAPER_INFO -- `date` \t" -depth 8 $PAPER_ID.png
 echo "Push to device"
 python push_img.py $PAPER_ID.png
