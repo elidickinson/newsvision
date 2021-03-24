5e356135d485b58712cedc399500f758e487c57e
Eli Dickinson <eli@industrydive.com>
2021-03-24 16:39:00 -0400

    add TZ & cleanup

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 #FROM alpine:3.13
 FROM python:3.8-alpine
-RUN apk add --no-cache poppler-utils imagemagick wget file
+RUN apk add --no-cache poppler-utils imagemagick wget file tzdata
 
 # Borrowed from https://github.com/kolyadin/pdf2img/blob/master/Dockerfile
 # Add standard microsoft webfonts: https://packages.debian.org/sid/ttf-mscorefonts-installer
--- paperme.sh
+++ paperme.sh
@@ -1,18 +1,26 @@
 #!/bin/sh
-# paperme.sh http://.... filename (.png will be added)
-# IL_CT - chicago trib
-# DC_WP - Wapo
+# paperme.sh DC_WP 
+# will load Washington Post
+
 # cd "${0%/*}"
+# If a paper_id was passed in (e.g. "DC_WP") then use that, otherwise grab one at random from paperindex.tsv
 PAPER_ID=${1:-`shuf -n 1 paperindex.tsv | awk '{print $1}'`}
+# Look up the other info for this paper_id (full name, location)
 PAPER_INFO=`grep "$PAPER_ID" paperindex.tsv`
 echo "$PAPER_INFO"
+# download the PDF for this paper for today's date, based on timezone as specified in env.list
+# this could fail if file doesn't exist yet
 wget "https://cdn.freedomforum.org/dfp/pdf`date "+%-d"`/$PAPER_ID.pdf" -q -O $(pwd)/$PAPER_ID.pdf
 echo "Render pdf to image"
-# echo docker run -it --rm -v $(pwd):/var/workdir/ kolyadin/pdf2img -png -singlefile -cropbox $1.pdf $1
-# docker run -it --rm -v $(pwd):/var/workdir/ kolyadin/pdf2img -png -singlefile -cropbox $1.pdf $1
+# convery to .png
 pdftoppm -png -singlefile -cropbox $PAPER_ID.pdf $PAPER_ID
 echo "Resize and annotate image"
-mogrify -gravity north -depth 8 -resize 1440x2560 -background white -extent 1440x2560 \
-    -font TimesNewRoman -undercolor '#F0F0F090' -fill black -gravity South -pointsize 18 -annotate +0+5 "\t $PAPER_INFO -- `date` \t" -depth 8 $PAPER_ID.png
+# resize to a little narrower than the actual resolution of 1440x2560 and pad with a white background
+# then also add a footer with the paper info to the bottom
+# and finally don't forget to specify 8-bit color depth since it doesn't like 24-bit images
+mogrify \
+	-gravity Center -resize 1430x2560 -background white -extent 1440x2560 \
+    -font Times-New-Roman -undercolor '#F0F0F090' -fill black -gravity South -pointsize 16 -annotate +0+5 " $PAPER_INFO  Loaded `date "+%Y-%m-%d %r"` " \
+    -depth 8 $PAPER_ID.png
 echo "Push to device"
 python push_img.py $PAPER_ID.png
